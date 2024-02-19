925a5ea67ec6767149689b06735a62052404854e
Caryn <65773680+ChenXiaoYingCaryn@users.noreply.github.com>
2024-02-19 10:04:17 +0800

    first commit

---
+++ src/main/java/com/example/cocktailnotes/CocktailNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.cocktailnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocktailNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cocktailnotes/CocktailNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cocktailnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
