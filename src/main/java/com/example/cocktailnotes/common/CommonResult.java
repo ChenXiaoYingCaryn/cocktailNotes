4e8662e97aa555f8ebbbc65292218e03552b4bc9
Caryn <65773680+ChenXiaoYingCaryn@users.noreply.github.com>
2024-03-01 00:59:34 +0800

    feature 用户注册

--- src/main/java/com/example/cocktailnotes/common/CommonResult.java
+++ src/main/java/com/example/cocktailnotes/common/CommonResult.java
@@ -17,7 +17,6 @@ public class CommonResult<T> {
     /**
      * 成功返回结果
      *
-     * @param data 获取的数据
      */
     public static <T> CommonResult<T> success() {
         return new CommonResult<T>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(), null);
